b3b30d33c5a3da3ca2141a27cdf0739990558bfb
Andrei Borovik <andrei.borovik2000@yandex.by>
2020-05-31 01:05:16 +0300

    The fourth commit

--- Defragger.h
+++ Defragger.h
@@ -3,11 +3,16 @@
 #include <Windows.h>
 class Defragger
 {
-	PRETRIEVAL_POINTERS_BUFFER out_buf;
+	int StartLCN = 0; //начальный кластер для перемещения файла
+	int FreeCluster = 0; //количество свободных кластеров для перемещения
+	int UseCluster = 0; //количество занятых кластеров файлом
+	int PrevVCN = 0;
+	PRETRIEVAL_POINTERS_BUFFER out_buf = 0;
 public:
 	Defragger();
 	~Defragger();
-	PRETRIEVAL_POINTERS_BUFFER getFileMap(LPCTSTR file);
+	void getFileMap(LPCTSTR file);
 	PVOLUME_BITMAP_BUFFER getFileVolume();
+	void FileMove(LPCSTR FileName);
 };
 
